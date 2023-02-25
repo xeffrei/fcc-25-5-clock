e60ed3e7e9ce9fdc5cf5756cdef255e6d8c3b890
Jeff Alexander <12126499+alexander-jeff@users.noreply.github.com>
2023-02-25 11:27:19 -0800

    Add session-label element
    - resolves #2
    - tests the same
    - fcc test #2 passing
    
    Refactor break and session elements into functional react fragments to
    make it easier to manage state later on. These may become their own features
    with access to a global state.

--- src/features/PomodoroTimer/pomodoroTimer.spec.js
+++ src/features/PomodoroTimer/pomodoroTimer.spec.js
@@ -7,4 +7,10 @@ describe('PomodoroTimer', () => {
     expect(getByTestId(/break-label/i)).toHaveAttribute('id', 'break-label')
     expect(getByTestId(/break-label/i)).toHaveTextContent('Break Length')
   })
+
+  it('contains an element with id session-label, that contains string Session Length', () => {
+    const { getByTestId } = render(<PomodoroTimer />)
+    expect(getByTestId(/session-label/i)).toHaveAttribute('id', 'session-label')
+    expect(getByTestId(/session-label/i)).toHaveTextContent('Session Length')
+  })
 })
