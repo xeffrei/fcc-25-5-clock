5f37be878e89120f16b95a80af09f42dd2bbffa7
Jeff Alexander <12126499+alexander-jeff@users.noreply.github.com>
2023-02-25 11:27:19 -0800

    Add session-label element
    - resolves #2
    - tests the same
    - fcc test #2 passing
    
    Refactor break and session elements into functional react fragments to
    make it easier to manage state later on. These may become their own features
    with access to a global state.

--- src/features/PomodoroTimer/PomodoroTimer.jsx
+++ src/features/PomodoroTimer/PomodoroTimer.jsx
@@ -1,13 +1,36 @@
+import React, { Fragment } from 'react'
 export const PomodoroTimer = () => {
+  const sessionTracker = () => { // may become its own feature/component later
+    return (
+      <>
+        <div
+          data-testid='session-label'
+          id='session-label'
+        >
+          Session Length
+        </div>
+      </>
+    )
+  }
+
+  const breakTracker = () => { // may become its own feature/component later
+    return (
+      <>
+        <div
+          data-testid='break-label'
+          id='break-label'
+        >
+          Break Length
+        </div>
+      </>
+    )
+  }
+
   return (
     <div data-testid='pomodoro-timer'>
       <h1>Pomodoro Timer</h1>
-      <div
-        data-testid='break-label'
-        id='break-label'
-      >
-        Break Length
-      </div>
+      {breakTracker()}
+      {sessionTracker()}
     </div>
   )
 }
