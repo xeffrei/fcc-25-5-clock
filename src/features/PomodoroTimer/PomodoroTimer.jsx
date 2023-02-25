f6bc6b3468616d0676f626c3dbe43964cb2ae39f
Jeff Alexander <12126499+alexander-jeff@users.noreply.github.com>
2023-02-25 10:48:21 -0800

    Add break-label element resolves #1
    - Test the same
    - Add PomodoroTimer feature (parent of break-label)
    - Enable fcc test-harness
      - [x] Tests 1 and 2 pass
    - resolves #1

---
+++ src/features/PomodoroTimer/PomodoroTimer.jsx
@@ -0,0 +1,13 @@
+export const PomodoroTimer = () => {
+  return (
+    <div data-testid='pomodoro-timer'>
+      <h1>Pomodoro Timer</h1>
+      <div
+        data-testid='break-label'
+        id='break-label'
+      >
+        Break Length
+      </div>
+    </div>
+  )
+}
